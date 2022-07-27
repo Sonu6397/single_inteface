f1316a6b2bfe342688b96a22e3ee03e7c5bdba88
admin <admin@CHETUIWK1860>
2022-07-27 11:34:57 +0530

    Add project files.

---
+++ single_inteface/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace single_inteface
+{
+    interface test1
+    {
+        public void Show();
+    }
+
+    class demo : test1
+    {
+        public void Show()
+        {
+            Console.WriteLine("this method id Show");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            demo d = new demo();
+            d.Show();
+            Console.ReadLine();
+        }
+    }
+}
